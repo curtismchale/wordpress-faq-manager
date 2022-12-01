c72da965304630c73a988e7d0a15fc2e9d6ad661
Curtis McHale <curtis@curtismchale.ca>
2022-12-01 11:03:02 -0800

    updated to use laravel mix for compiling

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,7 @@
 
 let mix = require( 'laravel-mix' );
 
-mix.js( 'lib/js/faq.admin.js', 'lib/js/' );
+mix.minify( 'lib/js/faq.admin.js' )
+	.minify( 'lib/js/faq.front.js' )
+	.minify( 'lib/css/faq.admin.css' )
+	.minify( 'lib/css/faq.front.css' );
